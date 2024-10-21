81c45861ae7358a9ea881323243d5d18969a9911
Alisa Petrova <60570090+sn0rkmaiden@users.noreply.github.com>
2024-10-21 23:52:41 +0300

    added more description

--- lib/card2.dart
+++ lib/card2.dart
@@ -28,6 +28,21 @@ class CardData2 {
   });
 }
 
+final Uri _url1 = Uri.parse(
+    'https://web.archive.org/web/20100502013959/http://www.concentric.net/~pvb/ALG/rightpaths.html');
+
+final Uri _url2 = Uri.parse(
+    'https://www.youtube.com/watch?v=IUC-8P0zXe8');
+
+final Uri _url3 = Uri.parse(
+    'https://www.raulprisacariu.com/lills-method/');
+
+Future<void> _launchUrl(Uri uri) async {
+  if (!await launchUrl(uri)) {
+    throw Exception('Could not launch $uri');
+  }
+}
+
 class CardScreen2 extends StatelessWidget {
   const CardScreen2({required this.data, super.key});
 
@@ -35,22 +50,42 @@ class CardScreen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l = AppLocalizations.of(context);
     return Stack(
       children: [
         Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
             child: Column(children: [
               const Spacer(flex: 1),
-              Flexible(flex: 10, child: data.image!),
-              const Spacer(flex: 1),
+              if (data.image != null) Flexible(flex: 10, child: data.image!),
+              if (data.image != null) const Spacer(flex: 1),
               TeXView(
-                child: TeXViewDocument(data.latex),                
-                style: TeXViewStyle(textAlign: TeXViewTextAlign.center, contentColor: data.titleColor, fontStyle: TeXViewFontStyle(fontSize: 20)),
-              ),                                      
+                child: TeXViewDocument(data.latex),
+                style: TeXViewStyle(
+                    textAlign: TeXViewTextAlign.center,
+                    contentColor: data.titleColor,
+                    fontStyle: TeXViewFontStyle(fontSize: 20)),
+              ),
               const Spacer(flex: 1),
-              TeXView(
-                child: TeXViewDocument(data.text),                
-                style: TeXViewStyle(textAlign: TeXViewTextAlign.center, contentColor: data.subtitleColor, fontStyle: TeXViewFontStyle(fontSize: 20)),
+              if (data.index == 4)...[
+                ElevatedButton(
+                  onPressed: () => _launchUrl(_url1),
+                  child: Text(l!.linkArticle),
+                ),
+                ElevatedButton(
+                  onPressed: () => _launchUrl(_url2),
+                  child: Text(l!.linkVideo),
+                ),
+                ElevatedButton(
+                  onPressed: () => _launchUrl(_url3),
+                  child: Text(l.linkWebsite),
+                )],
+              if (data.index != 4) TeXView(
+                child: TeXViewDocument(data.text),
+                style: TeXViewStyle(
+                    textAlign: TeXViewTextAlign.center,
+                    contentColor: data.subtitleColor,
+                    fontStyle: TeXViewFontStyle(fontSize: 20)),
               ),
               const Spacer(flex: 5)
             ]))
